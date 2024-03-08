a70f0431043c1d90e297e7de5e5d4210df2933b7
Makoto Hashimoto <makoto.hashimoto@quinyx.com>
2024-03-08 14:47:45 +0100

    Initial commit adding spring boot

---
+++ src/main/java/com/makotovh/cucumerpresentation/CucumerPresentationApplication.java
@@ -0,0 +1,13 @@
+package com.makotovh.cucumerpresentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CucumerPresentationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CucumerPresentationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/makotovh/cucumerpresentation/CucumerPresentationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.makotovh.cucumerpresentation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CucumerPresentationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
